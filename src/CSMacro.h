93d3b44276999f728f196388d261adedc183d377
wanghz <h8d9123@163.com>
2016-09-22 22:13:59 +0800

    add weapon class

--- src/CSMacro.h
+++ src/CSMacro.h
@@ -1,5 +1,21 @@
 #pragma once
 namespace cs
 {
+	/*
+	*defines resource handle
+	*/
 	typedef unsigned int HMODLE;
+	typedef unsigned int HWEAPON;
+	typedef unsigned int HSOUND;
+	typedef unsigned int HICON;
+	typedef unsigned int HBITMAP;
+	typedef unsigned int HMESH;
+	typedef unsigned int HLIGHT;
+	typedef unsigned int HMATERIAL;
+
+
+	/*
+	*defines data types
+	*/
+	typedef CSPoint CSVector3;
 }
