538c6916f595ad7fb5441c373ea3754617f34fbc
wanghz <h8d9123@163.com>
2017-07-11 22:21:48 +0800

    a litttle modifty

--- src/CSEngine.cpp
+++ src/CSEngine.cpp
@@ -1,4 +1,4 @@
-template<T>
+template<class T>
 class CSVector3
 {
 public:
--- src/CSMacro.h
+++ src/CSMacro.h
@@ -1,6 +1,7 @@
 #pragma once
 namespace cs
 {
-	
-	
+#define ADD_GET(type, member) type Get##member(){return this->member;};
+#define ADD_SET(type, member) type Set##member(){this->member = member;};
+#define ADD_GET_SET(type, member) ADD_GET(type,member);ADD_SET(type,member);
 }
