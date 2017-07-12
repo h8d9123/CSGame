6a44d802fcb9e5523f26992e25e4bda5d9c15f43
wanghz <h8d9123@163.com>
2017-07-12 12:03:35 +0800

    add macros about release and delete operation

--- src/CSMacro.h
+++ src/CSMacro.h
@@ -4,4 +4,8 @@ namespace cs
 #define ADD_GET(type, member) type Get##member(){return this->member;};
 #define ADD_SET(type, member) type Set##member(){this->member = member;};
 #define ADD_GET_SET(type, member) ADD_GET(type,member);ADD_SET(type,member);
+#define V_RETURN(exptr) {HRESULT hr=exptr;if (FAILED(hr)) return hr;};
+#define SAFE_RELEASE(p) {if(p) {(p)->Release();(p)=nullptr;}};
+#define SAFE_DELETE() {if(p){delete  (p);(p)=nullptr}};
+#define SAFE_DELETE_ARRAY {if(p){delete[] (p);(p)=nullptr;}};
 }
