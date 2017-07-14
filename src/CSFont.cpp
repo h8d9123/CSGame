5a1e44c1400f759efc19b1280f2aed6b47f909fc
wanghz <h8d9123@163.com>
2017-07-14 17:30:36 +0800

    modify csfont construct fun

--- src/CSFont.cpp
+++ src/CSFont.cpp
@@ -1,14 +1,22 @@
 #include"CSFont.h"
 
-HRESULT cs::CSFont::Init(LPDIRECT3DDEVICE9 device)
+cs::CSFont::CSFont(LPDIRECT3DDEVICE9 pDevice) :pDevice(pDevice), height(0), width(0), weight(0), mipLevels(0)
+, italic(0), charSet(0), outputPrecision(0), quality(0), pitchAndFamily(0),
+pFacename(L"Arial"), pFont(nullptr) {
+	Init();
+};
+HRESULT cs::CSFont::Init()
 {
-	if (pFont) SAFE_RELEASE(pFont);
-	V_RETURN(D3DXCreateFont(device,height,width,weight,
-		mipLevels,italic,charSet,outputPrecision,quality,
-		pitchAndFamily, pFacename, &pFont));
-	pDevice = device;
-	return S_OK;
+	if (nullptr != pDevice) {
+		V_RETURN(D3DXCreateFont(pDevice, height, width, weight, mipLevels, italic, charSet, outputPrecision,
+			quality, pitchAndFamily, pFacename, &pFont));
+	}
+	return E_FAIL;
 }
 void cs::CSFont::Destroy() {
 	if (pFont) SAFE_RELEASE(pFont);
+}
+HRESULT cs::CSFont::UpDate() {
+	SAFE_RELEASE(pFont);
+	return Init();
 }
--- src/CSFont.h
+++ src/CSFont.h
@@ -24,11 +24,16 @@ namespace cs {
 		LPCTSTR pFacename;
 		LPD3DXFONT pFont;
 	public:
+		/*
 		CSFont() :pDevice(nullptr), height(0), width(0), weight(0), mipLevels(0)
 			, italic(0), charSet(0), outputPrecision(0), quality(0), pitchAndFamily(0),
 			pFacename(L"Arial"), pFont(nullptr) {};
+		*/
 
-		HRESULT Init(LPDIRECT3DDEVICE9 device);
+		CSFont(LPDIRECT3DDEVICE9 pDevice);
+
+		HRESULT Init();
+		HRESULT UpDate();
 		void Destroy();
 		//add functions to get or set members;
 		ADD_GET_SET(LPDIRECT3DDEVICE9, Device, pDevice);
@@ -43,30 +48,36 @@ namespace cs {
 		ADD_GET_SET(DWORD, PitchAndFamily, pitchAndFamily);
 		ADD_GET_SET(LPCTSTR, PFacename, pFacename);
 		ADD_GET(LPD3DXFONT, Instance, pFont);
-
 	};
 	class CSText {
 	protected:
 		CSFont *pfont;
 		LPD3DXSPRITE pSprite;
 		LPCTSTR pText;
-		int Count;
+		int count;
 		RECT rect;
 		DWORD format;
 		D3DCOLOR color;
 	public:
-		CSText(CSFont* pfont) :pfont(pfont), pText(L"Hello"), Count(-1), rect({ 0, 0, 20, 100 }), format(DT_TOP | DT_LEFT), color(0xffffffff) {}
+		ADD_GET_SET(LPD3DXSPRITE,PSprite, pSprite)
+		ADD_GET_SET(int, Count, count);
+		ADD_GET_SET(DWORD, Format, format);
+		ADD_GET_SET(D3DCOLOR, Color,color);
+
+	public:
+		CSText(CSFont* pfont) :pfont(pfont), pText(L"Hello"), count(-1), rect({ 0, 0, 600, 20 }), format(DT_TOP | DT_LEFT), color(0xffffffff) {}
 		void DrawText(LPCTSTR pText){
 			ID3DXFont* font;
 			font = pfont->GetInstance();
-			font->DrawText(pSprite, pText, Count, &rect, format, color);
+			font->DrawText(pSprite, pText, count, &rect, format, color);
 
 		}
 		void DrawText(LPD3DXSPRITE pSprite, LPCTSTR pText, RECT rect, D3DCOLOR color=0xffffffff) {
 			ID3DXFont* font;
 			font = pfont->GetInstance();
-			font->DrawText(pSprite, pText, Count, &rect, format, color);
+			font->DrawText(pSprite, pText, count, &rect, format, color);
 		}
+
 	};
 
 }
